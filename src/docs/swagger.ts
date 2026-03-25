01c4fdc9299ef4b8e5c6123888e4bfef7c92f08b
Annette Carvalho <varelannette0@gmail.com>
2026-03-25 12:23:13 -0100

    doc. swagger

---
+++ src/docs/swagger.ts
@@ -0,0 +1,28 @@
+import swaggerJsdoc from 'swagger-jsdoc'
+import path from 'path'
+
+const options: swaggerJsdoc.Options = {
+    definition: {
+        openapi: `3.0.0`,
+        info: {
+            title : "API servidor Local",
+            description: `Plataforma de Gestão de Prestadores e Serviços`,
+            version: `1.0.0`
+        },
+        servers: [
+            {
+                url: `http://localhost:8080`,
+                description: `dev`,
+            }
+        ],
+
+        apis: [
+            path.join(process.cwd(), "./src/docs/schemas/*.yaml"),
+            path.join(process.cwd(), "./src/docs/paths/*.yaml")
+
+        ]
+
+    }
+}
+
+export const swaggerSpec = swaggerJsdoc(options);
