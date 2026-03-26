d0dd59e0aef451694b667474b338259d6f372c54
Annette Carvalho <varelannette0@gmail.com>
2026-03-26 12:39:42 -0100

    continuacao, swagger

--- src/docs/swagger.ts
+++ src/docs/swagger.ts
@@ -16,13 +16,12 @@ const options: swaggerJsdoc.Options = {
             }
         ],
 
+         },
         apis: [
             path.join(process.cwd(), "./src/docs/schemas/*.yaml"),
             path.join(process.cwd(), "./src/docs/paths/*.yaml")
 
         ]
-
-    }
 }
 
 export const swaggerSpec = swaggerJsdoc(options);
